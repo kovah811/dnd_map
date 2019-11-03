b462c1f25eef0b3fbe03a9e5752b37082c72bff7
kovah811 <36887070+kovah811@users.noreply.github.com>
2019-11-03 16:29:02 +0000

    Add set/check password to user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app import db
 
 
@@ -9,3 +11,9 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
