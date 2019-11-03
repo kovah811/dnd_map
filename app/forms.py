e0d49c28eccc43615f65e1ab4328d5db6aaf0e81
kovah811 <36887070+kovah811@users.noreply.github.com>
2019-11-03 16:58:42 +0000

    add login/logout

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,13 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Float
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
+
+
 class AddMarkerForm(FlaskForm):
     location_name = StringField('Location name', validators=[DataRequired()])
     wiki_link = StringField('Wiki link')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, request, jsonify, flash
+from flask import render_template, redirect, jsonify, flash, url_for
+from flask_login import current_user, login_user, logout_user
 
 from app import app
-from app.forms import AddMarkerForm
+from app.forms import AddMarkerForm, LoginForm
+from app.models import User
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,12 +15,23 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Log in', form=form)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/addMarker', methods=['POST'])
